930197123bfe0ee56aa329bb61bc0ad95fc2080b
Jeremy Leland <jeremy.leland@gmail.com>
2024-12-04 15:33:24 -0800

    Reorder and delete blocks

--- src/Level.js
+++ src/Level.js
@@ -11,13 +11,17 @@ export const Edit = {
     apply: ( level, cmd ) => addBlock( level, cmd.index, cmd.block ),
     undo:  ( level, cmd ) => deleteBlock( level, cmd.index ),
   },
+  DeleteBlock: {
+    apply: ( level, cmd ) => deleteBlock( level, cmd.index ),
+    undo:  ( level, cmd ) => addBlock( level, cmd.index, cmd.block ),
+  },
   ChangeBlockBounds: {
     apply: ( level, cmd ) => changeBlockBounds( level, cmd.index, cmd.dBounds ),
     undo:  ( level, cmd ) => changeBlockBounds( level, cmd.index, cmd.dBounds.map( b => -b ) ),
   },
-  DeleteBlock: {
-    apply: ( level, cmd ) => deleteBlock( level, cmd.index ),
-    undo:  ( level, cmd ) => addBlock( level, cmd.index, cmd.block ),
+  ReorderBlock: {
+    apply: ( level, cmd ) => reorderBlock( level, cmd.index, cmd.dIndex ),
+    undo:  ( level, cmd ) => reorderBlock( level, cmd.index, -cmd.dIndex ),
   },
 };
 
@@ -31,4 +35,8 @@ function deleteBlock( level, index ) {
 
 function changeBlockBounds( level, index, dBounds ) {
   dBounds.forEach( ( b, bIndex ) => level.blocks[ index ].bounds[ bIndex ] += b );
+}
+
+function reorderBlock( level, index, dIndex ) {
+  level.blocks.splice( index + dIndex, 0, ...level.blocks.splice( index, 1 ) )
 }
